e376d620ccf8109ffbc145174a273974ed9d694d
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-01 22:27:42 -0400

    fixed bugs and typos in schema and seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,25 @@ CREATE DATABASE hogwarts_db;
 USE hogwarts_db;
 
 CREATE TABLE departments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id),
-    ON DELETE SET NULL
+    salary DECIMAL(10,2),
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -13,28 +13,28 @@ VALUES
     ('005','Caretaker', 50000, '001'),
     ('006','Gameskeeper', 75000, '001'),
     ('007','Ghost', 0, '001'),
-    ('008', 'Ministry_Worker', 75000, '005');
+    ('008', 'Ministry_Worker', 75000, '003');
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-    ('001','Albus','Dumbledore','001','002'), -- headmaster --
-    ('002','Cornelius','Fudge','004',''), -- minister of magic --
-    ('003','Severus','Snape','002','001'), -- head of slytherin --
-    ('004','Minerva','McGonagall','002','001'), -- head of gryffindor --
-    ('005','Remus','Lupin','002','001'), -- defense against the dark arts --
-    ('006','Pomona','Sprout','002','001') -- head of hufflepuff --
-    ('007','Filius','Flitwick','002','001') -- head of ravenclaw --
-    ('008','Rubeus','Hagrid','006','001'), -- gameskeeper --
-    ('009','Argus','Filch','005','001'), -- caretaker(squib) --
-    ('010','Sybill','Trelwaney','002','001'), -- divination --
-    ('011','Dolores','Umbridge','008','002'), -- ministry worker --
-    ('012','Arthur','Weasley','008','002'), -- ministry worker --'
-    ('013','Harry','Potter','003','004') -- student --
+    ('001','Cornelius','Fudge','004','001'), -- minister of magic --
+    ('002','Albus','Dumbledore','001','001'), -- headmaster --
+    ('003','Severus','Snape','002','002'), -- head of slytherin --
+    ('004','Minerva','McGonagall','002','002'), -- head of gryffindor --
+    ('005','Remus','Lupin','002','002'), -- defense against the dark arts --
+    ('006','Pomona','Sprout','002','002'), -- head of hufflepuff --
+    ('007','Filius','Flitwick','002','002'), -- head of ravenclaw --
+    ('008','Rubeus','Hagrid','006','002'), -- gameskeeper --
+    ('009','Argus','Filch','005','002'), -- caretaker(squib) --
+    ('010','Sybill','Trelwaney','002','002'), -- divination --
+    ('011','Dolores','Umbridge','008','001'), -- ministry worker --
+    ('012','Arthur','Weasley','008','001'), -- ministry worker --
+    ('013','Harry','Potter','003','004'), -- student --
     ('014','Ron','Weasley','003','004'), -- student --
     ('015','Hermione','Granger','003','004'), -- student --
     ('016','Neville','Longbottom','003','004'), -- student --
     ('017','Luna','Lovegood','003','007'), -- student --
     ('018','Draco','Malfoy','003','003'), -- student --
     ('019','Cedric','Diggory','003','006'), -- student --
-    ('020','Nearly-Headless','Nick','007','001') -- ghost --
-    ('021','Moaning','Myrtle','007','001') -- ghost --
+    ('020','Nearly-Headless','Nick','007','002'), -- ghost --
+    ('021','Moaning','Myrtle','007','002'); -- ghost --
